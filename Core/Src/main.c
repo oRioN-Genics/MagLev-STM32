eceef252775a6ac62a46e0b1a9520e0b2e87ae2f
SaTh <Sathsara2021@gmail.com>
2026-04-15 22:18:44 +0530

    test: using SysTick for precise delays

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,4 +1,21 @@
+#include "stm32f103xb.h"
 #include "stm32f1xx.h"
+#include <stdint.h>
+
+void delay_ms (uint32_t ms) {
+    SysTick->LOAD = 8000 - 1;
+    SysTick->VAL = 0; // set current to 0, forcing reset
+    // set bit 2 (clock) to 1 for cpu clock and bit 0 to 1 for enable
+    SysTick->CTRL = (1 << 2) | (1 << 0);
+
+    for (uint32_t i = 0; i < ms; i++) {
+        while ((SysTick->CTRL & (1 << 16)) == 0) {
+
+        }
+    }
+
+    SysTick->CTRL = 0;
+}
 
 int main (void) {
     RCC->APB2ENR |= (1 << 4);
@@ -10,9 +27,6 @@ int main (void) {
 
     while (1) {
         GPIOC->ODR ^= (1 << 13); // toggle using XOR
-
-        for (volatile uint32_t i = 0; i < 500000; i++) {
-            // do nothing
-        }
+        delay_ms(1000);
     }
 }
