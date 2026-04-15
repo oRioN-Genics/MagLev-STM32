f81ba2a63537dfddb70320180ea602f1d6b47e27
SaTh <Sathsara2021@gmail.com>
2026-04-16 03:22:29 +0530

    feat(pwm): add driver functions for pwm signal generation with duty cycle control

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,32 +1,13 @@
 #include "stm32f103xb.h"
 #include "stm32f1xx.h"
+#include "pwm.h"
 #include <stdint.h>
 
-void delay_ms (uint32_t ms) {
-    SysTick->LOAD = 8000 - 1;
-    SysTick->VAL = 0; // set current to 0, forcing reset
-    // set bit 2 (clock) to 1 for cpu clock and bit 0 to 1 for enable
-    SysTick->CTRL = (1 << 2) | (1 << 0);
-
-    for (uint32_t i = 0; i < ms; i++) {
-        while ((SysTick->CTRL & (1 << 16)) == 0) {
-
-        }
-    }
-
-    SysTick->CTRL = 0;
-}
-
 int main (void) {
-    RCC->APB2ENR |= (1 << 4);
-
-    // this CRH has to be set to 0 first bcz they are not 0 originaly
-    GPIOC->CRH &= ~(0xF << 20);
-    // CRF = 00 and MODE = 10. hence, 0x2 = 0010. making this GPIO push pull o/p
-    GPIOC->CRH |= (0x2 << 20);
+    PWM_Init();
+    PWM_SetDutyCycle(uint16_t 500);
 
     while (1) {
-        GPIOC->ODR ^= (1 << 13); // toggle using XOR
-        delay_ms(1000);
+        
     }
 }
